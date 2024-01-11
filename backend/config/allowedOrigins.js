de72c39900b6bf84df1c5588864b5d2541d06740
ChristianFezard <christian.fezard@gmail.com>
2024-01-11 15:48:41 +0100

    Initial commit

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,9 @@
+// allowing access to the API
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com', //placeholder
+    'https://dandrepairshop.com' // placeholder
+]
+
+module.exports = allowedOrigins
